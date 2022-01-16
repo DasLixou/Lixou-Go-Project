7f184178633596b31a4396ce1a16354ad24e93b1
DasLixou <einlixou@gmail.com>
2022-01-16 19:51:17 +0100

    ✍️ Added Scanf to Console

--- helloWorld.go
+++ helloWorld.go
@@ -5,5 +5,10 @@ import (
 )
 
 func main() {
+	var feeling string
+
 	fmt.Println("Hello World, welcome to Lixou's GoLang Test Project 😄")
+	fmt.Print("How do u feel? -> ")
+	fmt.Scanf("%s", &feeling)
+	fmt.Println("So u are feeling " + feeling + "?")
 }
